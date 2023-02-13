2a0f778c826689fb2bc42cc5b8baad76a70fe5f7
Ohad Orbach <ohadorbach92@gmail.com>
2023-02-13 12:37:21 +0200

    update manager screen

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/EmployeesMainWindow.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/EmployeesMainWindow.java
@@ -5,10 +5,14 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
 
 import java.io.IOException;
@@ -47,30 +51,35 @@ public class EmployeesMainWindow {
     private Button button6;
 
     @FXML
-    public void changeMode(ActionEvent event) {
+    public void changeMode(ActionEvent event){
         PrimaryController.ChangeForAll(parent, imMode);
     }
 
     @FXML
     public void LogOut(ActionEvent event) throws IOException {
         SimpleClient myClient = SimpleClient.getClient();
-        myClient.sendToServer("logout employee:" + App.employee.getId());
-        App.history.remove(App.history.size() - 1);
-        App.setRoot(App.history.get(App.history.size() - 1));
+        myClient.sendToServer("logout employee:"+App.employee.getId());
+        App.history.remove(App.history.size()-1);
+        App.setRoot(App.history.get(App.history.size()-1));
     }
 
 
     @FXML
     void button1Func(ActionEvent event) throws IOException {
-        if (button1.getText().equals("Statistical Information")) {
+        if(button1.getText().equals("Statistical Information"))
+        {
             SimpleClient.getClient().sendToServer("#request: stastistical information list");
             System.out.println();
             App.setRoot("StastisticalInformation");
             App.history.add("StastisticalInformation");
             App.setRoot("StastisticalInformation");
-        } else if (button1.getText().equals("Report a parking fault")) {
+        }
+        else if(button1.getText().equals("Report a parking fault"))
+        {
 
-        } else {
+        }
+        else
+        {
             SimpleClient.getClient().sendToServer("#request: complaint table");
             App.history.add("ComplaintsEmployee");
             App.setRoot("ComplaintsEmployee");
@@ -80,15 +89,14 @@ public class EmployeesMainWindow {
     }
 
     @FXML
-    void button2Func(ActionEvent event) throws IOException {
+    void button2Func(ActionEvent event) throws IOException{
 
-        if (button1.getText().equals("Save Parking Spot")) {
+        if(button1.getText().equals("Save Parking Spot"))
+        {
 
-        } else if (button2.getText().equals("Make report")) {
-            App.history.add("MakeReport");
-            App.setRoot("MakeReport");
-
-        } else {
+        }
+        else
+        {
             App.history.add("ParkingScreenController");
             App.setRoot("ParkingScreenController");
         }
@@ -96,17 +104,14 @@ public class EmployeesMainWindow {
     }
 
     @FXML
-    void button3Func(ActionEvent event) throws IOException {
-        if (button3.getText().equals("View Reports")) {
-            App.history.add("ViewReport");
-            App.setRoot("ViewReport");
-        }
+    void button3Func(ActionEvent event) throws IOException{
+
     }
 
-    @FXML
-        // This method is called by the FXMLLoader when initialization is complete
-    void initialize() {
-        if (!isLightMode) {
+    @FXML // This method is called by the FXMLLoader when initialization is complete
+    void initialize()
+    {
+        if(!isLightMode){
             PrimaryController.setDarkMode(parent, imMode);
         }
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm:ss");
@@ -122,19 +127,18 @@ public class EmployeesMainWindow {
         // Initializing the gui according to the job.
         // 4 types of Employees: company manager,parking lot manager,parking lot worker,costumer service
         System.out.println(App.employee.getJob());
-        switch (App.employee.getJob()) {
+        switch(App.employee.getJob()){
             case "company manager":
                 button1.setText("Statistical Information");
                 button2.setText("Get Parking Lot Status");
-                button3.setText("View Reports");
+                button3.setVisible(false);
                 button4.setVisible(false);
                 button5.setVisible(false);
                 button6.setVisible(false);
                 break;
             case "parking lot manager":
                 button1.setText("Statistical Information");
-                button2.setText("Make report");
-                ;
+                button2.setVisible(false);;
                 button3.setVisible(false);
                 button4.setVisible(false);
                 button5.setVisible(false);
